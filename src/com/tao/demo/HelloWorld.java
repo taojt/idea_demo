c0c921edb5679fdb31dc0441c698cfa270f323ff
taojt <taojt08@163.com>
2016-05-28 00:09:50 +0800

    add message

--- src/com/tao/demo/HelloWorld.java
+++ src/com/tao/demo/HelloWorld.java
@@ -6,5 +6,6 @@ package com.tao.demo;
 public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello World !");
+        System.out.println("Hello,Java !");
     }
 }
