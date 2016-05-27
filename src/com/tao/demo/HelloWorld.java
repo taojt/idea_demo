c9a79b335f737f1cd53012f186e15608f26ff4f6
Jang Tao <Jang Tao>
2016-05-27 23:58:28 +0800

    Initial commit

---
+++ src/com/tao/demo/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.tao.demo;
+
+/**
+ * Created by taojt on 2016/5/27.
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello World !");
+    }
+}
