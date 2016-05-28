146d91aa140230d5b1555b0e2b823c8156558951
taojt <taojt08@163.com>
2016-05-29 00:35:42 +0800

    添加线程目录

--- src/com/tao/demo/thread/demo4/ThreadB.java
+++ src/com/tao/demo/thread/demo4/ThreadB.java
@@ -15,12 +15,12 @@
 			String curThreadName = curThread.getName();
 			System.out.println("这是线程的名称:"+curThread.getName());
 			System.out.println("返回当前线程"+curThreadName+"的线程组中活动线程的数目："+Thread.activeCount());
-			System.out.println("返回该线程"+curThreadName+"的标识符："+curThread.getId());
-			System.out.println("返回线程"+curThreadName+"的优先级:"+curThread.getPriority());
-			System.out.println("返回该线程"+curThreadName+"的状态:"+curThread.getState());
-			System.out.println("返回该线程"+curThreadName+"所属的线程组:"+curThread.getThreadGroup());
-			System.out.println("测试线程"+curThreadName+"是否处于活动状态:"+curThread.isAlive());
-			System.out.println("测试线程"+curThreadName+"是否测试该线程是否为守护线程:"+curThread.isDaemon());
+			System.out.println("返回该线程 "+curThreadName+"的标识符："+curThread.getId());
+			System.out.println("返回线程 "+curThreadName+"的优先级:"+curThread.getPriority());
+			System.out.println("返回该线程 "+curThreadName+"的状态:"+curThread.getState());
+			System.out.println("返回该线程 "+curThreadName+"所属的线程组:"+curThread.getThreadGroup());
+			System.out.println("测试线程 "+curThreadName+"是否处于活动状态:"+curThread.isAlive());
+			System.out.println("测试线程 "+curThreadName+"是否测试该线程是否为守护线程:"+curThread.isDaemon());
 		}
 		
 		
