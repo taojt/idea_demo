eae56230200471636d624f621fc824998ed08df5
taojt <taojt08@163.com>
2016-05-29 19:30:32 +0800

    add some files

--- HelloJ2SE/src/com/shiyanlou/CollectionFramework/GenerateLottery.java
+++ HelloJ2SE/src/com/shiyanlou/CollectionFramework/GenerateLottery.java
@@ -13,19 +13,19 @@ import java.util.Random;
  */
 public class GenerateLottery {
 
-    public List<String> getHeadNumber(){
+    public List<String> getHeadNumber() {
         List<String> list = new ArrayList<String>();
         String lotteryNumber = "";
-        for(int i = 1; i < 36; i++){
-            if (i < 10){
+        for (int i = 1; i < 36; i++) {
+            if (i < 10) {
                 list.add("0" + i + " ");
-            }else{
+            } else {
                 list.add("" + i + " ");
             }
         }
         int roundIndex = 0;
         List<String> lotteryList = new ArrayList<String>();
-        for(int i = 0;i < 5; i++){
+        for (int i = 0; i < 5; i++) {
             int amount = list.size();
             Random r = new Random();
             roundIndex = r.nextInt(amount);//获取一个从0 到amount-1 的随机整数
@@ -38,20 +38,20 @@ public class GenerateLottery {
 
     }
 
-    public List<String> getRearNumber(){
+    public List<String> getRearNumber() {
         List<String> list = new ArrayList<String>();
         String lotteryNumber = "";
-        for (int i = 1; i < 13; i++){
-            if (i < 10){
+        for (int i = 1; i < 13; i++) {
+            if (i < 10) {
                 list.add("0" + i + " ");
-            }else{
+            } else {
                 list.add("" + i + " ");
             }
         }
 
         int roundIndex = 0;
         List<String> lotteryList = new ArrayList<String>();
-        for (int i = 0; i < 2; i++){
+        for (int i = 0; i < 2; i++) {
             int amount = list.size();
             Random r = new Random();
             roundIndex = r.nextInt(amount);
@@ -63,18 +63,18 @@ public class GenerateLottery {
         return lotteryList;
     }
 
-    public void generateLottery(String groupNum){
+    public void generateLottery(String groupNum) {
         int groupNumber = 0;
         groupNumber = Integer.parseInt(groupNum);
         StringBuilder sbuilder = new StringBuilder(); // 创建字符串生成器对象，使用字符串生成器能够较为方便地在字符串中追加内容
-        for (int i = 0; i < groupNumber; i++){
+        for (int i = 0; i < groupNumber; i++) {
             List<String> startList = getHeadNumber();
             List<String> endList = getRearNumber();
-            for (int m = 0; m < startList.size(); m++){
+            for (int m = 0; m < startList.size(); m++) {
                 sbuilder.append(startList.get(m));
             }
             sbuilder.append("      ");
-            for (int m = 0; m < endList.size(); m++){
+            for (int m = 0; m < endList.size(); m++) {
                 sbuilder.append(endList.get(m));
             }
             sbuilder.append("\n");
